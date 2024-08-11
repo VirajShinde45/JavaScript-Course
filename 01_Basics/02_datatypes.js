54d0bc113b0d8f3dacb0671fee0d731097fdfe90
VirajShinde45 <virajshinde4545@gmail.com>
2024-08-11 10:16:09 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+let name = "Viraj"
+let age = 21
+
+/*
+*Types of Datatypes*
+number
+bigint
+string => ""
+boolean
+null => standalone value
+undefined => not assigned yet
+symbol => unique
+
+object
+*/
+
+console.log(typeof age);
+console.log(typeof "Viraj");
