dde836d0bd1ff4dfc6e84b1923ab4bb45192832b
VirajShinde45 <virajshinde4545@gmail.com>
2024-08-11 13:16:16 +0000

    comparison operations

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,12 @@
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2==1)
+console.log(2 != 1)
+
+console.log("2">1) //automatically converted to number
+console.log("02">1)
+
+console.log(null>0)
+console.log(null == 0)
+console.log(null>=0)
