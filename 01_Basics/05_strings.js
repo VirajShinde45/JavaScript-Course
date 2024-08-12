0b05cc9e5303774ec93bf65bd406f304be0febc0
VirajShinde45 <virajshinde4545@gmail.com>
2024-08-12 14:53:20 +0000

    strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,9 @@
+const name = "Viraj"
+const age = 21
+
+//console.log(name + age + "Shinde")
+console.log(`Hello my name is ${name} and my age is ${age}`); //use backticks(``) instead of single quotes('')
+
+//Declare String
+const surname = new String('Shinde')
+console.log(surname[2])
